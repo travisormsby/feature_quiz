5491f459ab66352d934f64da5c991cf58895f13f
Travis Ormsby <t.ormsby@esri.com>
2024-06-16 09:55:04 -0500

    Update main.py to fix problems with mutable default args

--- docs/main.py
+++ docs/main.py
@@ -1,3 +1,5 @@
+# PyScript version of the quiz, intended to be run in the browser
+
 import random
 from pyscript import document
 from pyweb import pydom
@@ -121,27 +123,30 @@ def random_open():
     open_mode = random.choice(["'r'", "'w'", "'a'"])
     return f"with open('{random_string()}.{random.choice(['csv', 'txt'])}', {open_mode}) as f:"
     
-default_func_list = [
-        ['numeric data type', random_int_or_float],
-        ['boolean data type', random_bool],
-        ['string data type', random_string],
-        ['list data type', random_list],
-        ['tuple data type', random_tuple],
-        ['access sequence elements by index', random_index_access],
-        ['dictionary data type', random_dict],
-        ['variable asignment', random_variable],
-        ['conditional', random_conditional],
-        ['loop over elements in sequence', random_loop],
-        ['function call', random_function_call],
-        ['function definition', random_function_def],
-        ['bring a module into your code', random_full_import],
-        ['bring a portion of a module into your code', random_from_import],
-        ['access file', random_open]
-]
+feature_dict = {
+        '1': ['Numeric data type', random_int_or_float],
+        '2': ['Boolean data type', random_bool],
+        '3': ['String data type', random_string],
+        '4': ['List data type', random_list],
+        '5': ['Tuple data type', random_tuple],
+        '6': ['Access sequence elements by index', random_index_access],
+        '7': ['Dictionary data type', random_dict],
+        '8': ['Variable asignment', random_variable],
+        '9': ['Conditional', random_conditional],
+        '10': ['Loop over elements in sequence', random_loop],
+        '11': ['Function call', random_function_call],
+        '12': ['Function definition', random_function_def],
+        '13': ['Bring in external modules', random_full_import],
+        '14': ['Bring in a portion of an external module', random_from_import],
+        '15': ['Access file', random_open]
+}
 
-# Purposefully mutating func list default arg
-# Make a copy of the default list to avoid mutating the orginal
-func_list = default_func_list[:]
+# Number of times you must correctly identify a feature to complete the game
+correct_needed = 1
+
+# Make a dict to hold the score for each feature
+# This dict will be purposefully mutated as a default argument
+score_dict = {k:0 for k, v in feature_dict.items()}
 
 def show_question(q_num, q_func):
     """
@@ -160,20 +165,20 @@ def show_question(q_num, q_func):
     else: 
         feature_div.innerText = q_func()
 
-def show_choices(func_list):
+def show_choices(choice_dict=feature_dict):
     """
     show the answer choices
     """
     options_div = document.querySelector("#options")
     options_html = "<ol>"
-    for func in func_list:
+    for func in choice_dict.values():
         options_html += f"<li>{func[0]}</li>"
     options_html += "</ol>"
     options_div.innerHTML = options_html
 
-def check_answer(event):
+def check_answer(event, feature_dict=feature_dict, score_dict=score_dict, correct_needed=correct_needed):
     """
-    check if the answer is correct
+    Update the score
     """
     answer = document.querySelector(".answer").value
     correct_answer = event.target.id
@@ -183,50 +188,69 @@ def check_answer(event):
 
     if answer is None or not answer.isnumeric() or answer != correct_answer:
         button.id = "incorrect"
-        response_div.innerText = f"{answer} is incorrect. The correct answer is: {correct_answer}. Click Continue to keep playing."
+        if answer in score_dict.keys():
+            score_dict[answer] -= 1
+            answer = feature_dict[answer][0] # convert numeric answer to function description
+        else:
+            random_answer = random.choice(list(score_dict.keys()))
+            score_dict[random_answer] -= 1
+        score_dict[correct_answer] -= 1
+        response_div.innerText = f"{answer} is incorrect. The correct answer is: {feature_dict[correct_answer][0]}. Click Continue to keep playing."
     else:
         button.id = correct_answer
-        response_div.innerText = (f"{answer} is correct! Click Continue to keep playing.")
+        score_dict[answer] += 1
+        response_div.innerText = (f"{feature_dict[answer][0]} is correct! Click Continue to keep playing.")
+    display_score(score_dict, correct_needed)
 
-def reset_display():
-    answer_input = pydom['.answer'][0]
-    answer_input.style["display"] = 'inline'
-    submit_button = pydom['.submit'][0]
-    submit_button.style["display"] = 'inline'
-    intro_div = pydom['#intro'][0]
-    intro_div.style["display"] = 'none'
-    response_div = document.querySelector('#response')
-    response_div.innerText = ""
-
-def mutate_func_list(event, func_list=func_list):
+def set_display():
     """
-    remove the correct answer from the function list
+    Remove the introduction / instructions
     """
-    if event.target.id.isnumeric():
-        if len(func_list) == 0:
-            # reset the func_list
-            func_list = default_func_list[:]
-        
-        func_list.pop(int(event.target.id)-1)
-        
-        if len(func_list) == 0:
-            # reset the func_list
-            func_list = default_func_list[:]
-        
-    return func_list
+    pydom['.answer'][0].style["display"] = 'inline'
+    pydom['.submit'][0].style["display"] = 'inline'
+    pydom['#intro'][0].style["display"] = 'none'
+    document.querySelector('.continue').innerText = "Continue"
+    document.querySelector('#response').innerText = ""
 
-def game(event, func_list=func_list):
+def display_score(score_dict, correct_needed):
+    """
+    display the score
+    """
+    score_div = document.querySelector("#score")
+    score = sum(score_dict.values())
+    score_div.innerText = f"Score: {score} / {len(score_dict) * correct_needed}"
+
+def game_complete():
+    """
+    Clean up the display when the game is complete
+    """
+    document.querySelector("#question").innerText = "You've completed the quiz! Refresh the page to play again."
+    document.querySelector("#feature").innerText = ""
+    document.querySelector("#options").innerHTML = ""
+    document.querySelector("#score").innerText = ""
+    document.querySelector(".continue").id = "reset"
+    pydom['.continue'][0].style["display"] = 'none'
+    pydom['.answer'][0].style["display"] = 'none'
+    pydom['.submit'][0].style["display"] = 'none'
+
+
+def game(event, score_dict=score_dict, feature_dict=feature_dict, correct_needed=correct_needed):
     """
     play the game
     """
-    reset_display()
-    func_list = mutate_func_list(event)
+    set_display()
+
+    if sum(score_dict.values()) == len(score_dict) * correct_needed:
+        game_complete()
+        score_dict = {k:0 for k, v in feature_dict.items()}
+
+        # Recursive means you can only play 999 games without refreshing browser
+        # This is an acceptable limitation
+        return
     
-    q_num = random.randint(1, len(func_list))
-    _, q_func = func_list[q_num-1]
+    q_num = random.choice([k for k,v in score_dict.items() if v < correct_needed])
+    _, q_func = feature_dict[q_num]
     
     show_question(q_num, q_func)
-    show_choices(func_list)
-
-    continue_button = document.querySelector(".continue")
-    continue_button.id = "incorrect"
+    show_choices()
+    display_score(score_dict, correct_needed)
